8e9756f79f02c982c9ee728967f7a2217d4eebdd
William <william.s@hedgue.com>
2024-02-09 14:38:43 -0800

    fix:empty panel placeholder text

--- components/shared/empty.tsx
+++ components/shared/empty.tsx
@@ -4,7 +4,7 @@ export default function Empty() {
   return (
     <div className="flex flex-col items-center justify-center pt-10 text-content-color">
       <PackageOpen className="mb-5 h-[40px] w-[40px]" />
-      <p className="text-lg">Data Not Found</p>
+      <p className="text-lg">No items yet</p>
     </div>
   );
 }
