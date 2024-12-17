754110fcb4d6291a1e86f9ed2bbf67a9de91b9d2
beewolf <biw725704@gmail.com>
2024-12-17 14:49:58 +0800

    fix: change dialog language

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 
 import { cn } from "@/lib/utils";
+import { useTranslations } from "next-intl";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -35,37 +36,39 @@ const DialogContent = React.forwardRef<
     showClose?: boolean | string;
     title?: string;
   }
->(({ className, children, showClose, title, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <DialogPrimitive.Content
-      ref={ref}
-      data-title={title ? "true" : "false"}
-      onOpenAutoFocus={(e) => e.preventDefault()}
-      className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4  rounded-lg border bg-background shadow-lg duration-200 data-[title=false]:p-6 data-[title=true]:pb-6 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
-        className,
-      )}
-      {...props}
-    >
-      {!!title && (
-        <DialogHeader className="relative rounded-t-lg border-b border-shadow-color bg-custom-bg-white">
-          <DialogTitle className="flex h-11 items-center justify-center text-base">
-            {title}
-          </DialogTitle>
-          {showClose && (
-            <DialogPrimitive.Close className="absolute right-4 top-[3px] rounded-sm opacity-100 transition-opacity hover:opacity-70 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-              <span className="text-sm text-primary">
-                {showClose === true ? "Close" : showClose}
-              </span>
-            </DialogPrimitive.Close>
-          )}
-        </DialogHeader>
-      )}
-      {children}
-    </DialogPrimitive.Content>
-  </DialogPortal>
-));
+>(({ className, children, showClose, title, ...props }, ref) => {
+  const T = useTranslations("Common");
+  return (
+    <DialogPortal>
+      <DialogOverlay />
+      <DialogPrimitive.Content
+        ref={ref}
+        data-title={title ? "true" : "false"}
+        onOpenAutoFocus={(e) => e.preventDefault()}
+        className={cn(
+          "fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4  rounded-lg border bg-background shadow-lg duration-200 data-[title=false]:p-6 data-[title=true]:pb-6 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+          className,
+        )}
+        {...props}
+      >
+        {!!title && (
+          <DialogHeader className="relative rounded-t-lg border-b border-shadow-color bg-custom-bg-white">
+            <DialogTitle className="flex h-11 items-center justify-center text-base">
+              {title}
+            </DialogTitle>
+            {showClose && (
+              <DialogPrimitive.Close className="absolute right-4 top-[3px] rounded-sm opacity-100 transition-opacity hover:opacity-70 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                <span className="text-sm text-primary">
+                  {showClose === true ? T("Close") : showClose}
+                </span>
+              </DialogPrimitive.Close>
+            )}
+          </DialogHeader>
+        )}
+        {children}
+      </DialogPrimitive.Content>
+    </DialogPortal>
+)});
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({
