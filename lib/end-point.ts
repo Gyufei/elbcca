9ac91cfeff3c2def507f0cc5ef162b7f0a1d9ec2
beewolf <biw725704@gmail.com>
2024-12-27 16:39:38 +0800

    fix: change withHost

--- lib/end-point.ts
+++ lib/end-point.ts
@@ -1,13 +1,5 @@
-const ProdHost = process.env.NEXT_PUBLIC_API_URL;
-const DevHost = "https://tafect-auth.aggregation.top";
-export const isPreview = process.env.NEXT_PUBLIC_IS_PREVIEW === "1";
-export const isProduction = process.env.NODE_ENV === "production";
-
-export const ApiHost = isProduction ? ProdHost : DevHost;
-// process.env.NEXT_PUBLIC_API_URL
-
 function WithHost(path: string) {
-  return `${ApiHost}${path}`;
+  return `${process.env.NEXT_PUBLIC_API_URL}${path}`;
 }
 
 export const SystemEndPointPathMap = {
