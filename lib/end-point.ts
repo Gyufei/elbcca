9d7f273034cfa7a3820278d9e6db541b4943dbb1
fei guo <812773673@qq.com>
2025-10-29 18:54:25 +0800

    fix:env-var

--- lib/end-point.ts
+++ lib/end-point.ts
@@ -1,4 +1,4 @@
-export const isPreview = process.env.NEXT_PUBLIC_IS_DETAKE_PREVIEW === "1";
+export const isPreview = process.env.NEXT_PUBLIC_IS_PREVIEW === "1";
 export const isProduction = process.env.NODE_ENV === "production" && !isPreview;
 
 const AuthHostUrl = isProduction
